a31be384994faf48878f6e4eac52ef672293fa78
nuzul <amuhnuzul@gmail.com>
2023-11-01 05:48:09 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+<<<<<<< HEAD
+# profile
+=======
+# deploy_profile
+>>>>>>> c130940c6dd7ad3841b626c9038d6645572faa12
+"# deploy_profile" 
---
+++ www/tentang.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simponi</title>
+
+    <!-- Latest compiled and minified CSS -->
+        <link href="asets/bootstrap.css" rel="stylesheet">
+        
+    <!-- Latest compiled JavaScript -->
+    <script src="asets/bootstrap.bundle.js"></script>
+
+    <!--CSS-->
+    <link rel="stylesheet" href="asets/resume.css">
+</head>
+<body>
+    <!--navbar-->
+    <div>
+            <nav class="navbar navbar-expand-lg navbar-dark shadow-sm" style="background-color: #000000">
+                <div class="container">
+                <a class="navbar-brand" href="#">Curriculum Vitae</a>
+
+                </div>
+            </nav>
+        </div>
+    <!--content-->
+        <div class="container text-bg-secondary">
+            <div class="row user-detail">
+            <div class=" text-center mb-3">
+                    <div>
+                        <h1>About Me</h1>
+                        <img src="images/foto alma latar merah.png" class="rounded-circle img-thumbnail" width="200">
+                        <h2>Andi Muhammad Nuzul</h2>
+                        <p><i class="fa fa-map-marker" aria-hidden="true"></i> Makassar,Indonesia.</p>
+                        <hr>
+                        <a href="https://github.com/amuhnuzul" class="btn btn-dark"><img src="https://iamkrishnaa.github.io/portofolio-website/images/Iconslogo/github-32.png"></a>
+                        <a href="https://www.linkedin.com/in/andi-muhammad-nuzul-492575205/" class="btn btn-dark" target="_blank">
+                          <img src="https://cdn.icon-icons.com/icons2/2248/PNG/512/linkedin_icon_135436.png" alt="LinkedIn" style="width: 30px; height: 30px;">
+                        </a>
+
+                        <hr>
+
+                            <div class="skill-section" id="skill-section">
+                       <h2>My Skills & Abilities</h2>
+                        <div class="skill-part">
+                        <div class="prgm-skill">
+                        <h2>Programming</h2>
+                            <p>Java</p>
+                            <div class="container-skill">
+                              <div class="skills java">60%</div>
+                            </div>
+                            
+                            <p>Dart</p>
+                            <div class="container-skill">
+                              <div class="skills dart">55%</div>
+                            </div>
+                            <p>Javascript</p>
+                            <div class="container-skill">
+                              <div class="skills js">60%</div>
+                            </div>
+                            <p>PHP</p>
+                            <div class="container-skill">
+                              <div class="skills php">50%</div>
+                            </div>
+                            <p>MySQL</p>
+                            <div class="container-skill">
+                              <div class="skills sql">70%</div>
+                            </div>
+                        </div>
+                        
+                        <div class="design-skill">
+                           <h2>Designing</h2>
+                            <p>HTML</p>
+                            <div class="container-skill">
+                              <div class="skills html">90%</div>
+                            </div>
+                            
+                            <p>CSS</p>
+                            <div class="container-skill">
+                              <div class="skills css">80%</div>
+                            </div>
+                            <p>Bootstrap</p>
+                            <div class="container-skill">
+                              <div class="skills bootstrap">70%</div>
+                            </div>
+                            <p>Photoshop</p>
+                            <div class="container-skill">
+                              <div class="skills photoshop">75%</div>
+                            </div>
+                            <p>Premiere Pro</p>
+                            <div class="container-skill">
+                              <div class="skills prepro">50%</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    <!--footer-->
+        <footer class="bg-dark  text-white">
+            <div class="text p-3" style="background-color: #000000;">
+            <p class="text-center"> © 2021 Copyright:
+                <a class="text-white" href="https://www.poliupg.ac.id/">andimuhnuzul</a></p>
+            </div>
+        </footer>
+</body>
+</html>
